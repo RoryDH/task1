b2b72a3ee8495e28635eeaf22cd8e1222f5a9a77
Rory Hughes <rory@roryhughes.net>
2017-11-20 23:39:45 +0000

    Add LCA method

--- lib/node.rb
+++ lib/node.rb
@@ -5,4 +5,22 @@ class Node
   def initialize(id, left: nil, right: nil)
     @id, @left, @right = id, left, right
   end
+
+  def self.lowest_common_ancestor(root, node_a, node_b)
+    return if root.nil?
+    return root if node_a == root || node_b == root
+
+    left  = lowest_common_ancestor(root.left, node_a, node_b)
+    right = lowest_common_ancestor(root.right, node_a, node_b)
+
+    if left && right
+      return root
+    end
+
+    if left
+      nil
+    else
+      right
+    end
+  end
 end
